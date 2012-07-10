7795448cd7bff3f589c3878367f4113e78b407e8
David Clark <evil@digitalfivesyndey.com>
2012-07-10 16:52:13 +1000

    filez

---
+++ testsite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace testsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? time)
+        {
+            return View();
+        }
+
+        public ActionResult SleepFor(int? time)
+        {
+            if ((time ?? 0) > 0)
+            {
+                System.Threading.Thread.Sleep(time.Value);
+                ViewBag.Message = string.Format("Slept for {0}ms", time);
+            }
+
+            return View("Sleep");
+        }
+
+        public ActionResult SleepRandom(int? min, int? max)
+        {
+            if(!min.HasValue || min.Value < 0) min = 50;
+            if(!max.HasValue || max.Value < min.Value) max = min + 200;
+            var rand = new System.Random();
+            return SleepFor(rand.Next(min.Value, max.Value));
+        }
+    }
+}
---
+++ testsite/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace testsite
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Random", "Random", new { controller = "Home", action = "SleepRandom" });
+            routes.MapRoute("RouteOne", "RouteOne", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteTwo", "RouteTwo", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteThree", "RouteThree", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteFour", "RouteFour", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteFive", "RouteFive", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteSix", "RouteSix", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteSeven", "RouteSeven", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteEight", "RouteEight", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteNine", "RouteNine", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("RouteTen", "RouteTen", new { controller = "Home", action = "SleepRandom", min = 0, max = 10 });
+            routes.MapRoute("Sleep", "TwoFifty", new { controller = "Home", action = "SleepFor", time = 250 });
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
